6a83bc2d1726cc6007934f291929d18b1780b330
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-21 01:21:56 +0900

    feat: モード切り替えボタンを追加

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "./logo";
+import { ModeToggle } from "./mode-toggle";
 
 export const Header = (): React.ReactNode => {
   return (
-    <header className="sticky top-0 z-50 p-6">
+    <header className="sticky top-0 z-50 p-4 flex gap-4 items-center justify-between">
       <Logo />
+      <ModeToggle />
     </header>
   );
 };
