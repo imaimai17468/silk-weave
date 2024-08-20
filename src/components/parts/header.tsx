62a408696ef4a49c80c4f3e1e34f227939f7b354
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-21 01:44:45 +0900

    feat: ヘッダーにグラスモーフィズムを追加

--- src/components/parts/header.tsx
+++ src/components/parts/header.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./mode-toggle";
 
 export const Header = (): React.ReactNode => {
   return (
-    <header className="sticky top-0 z-50 py-4 px-8 flex gap-4 items-center justify-between">
+    <header className="sticky top-0 z-50 py-4 px-8 flex gap-4 items-center justify-between bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
       <Logo />
       <ModeToggle />
     </header>
