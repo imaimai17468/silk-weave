cfb35a30b8dd262b086bfcadf4e62eab8d750c47
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-22 01:08:32 +0900

    feat: logoにアイコンを追加

--- src/components/parts/logo.tsx
+++ src/components/parts/logo.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export const Logo = (): React.ReactNode => {
-  return <p className="text-2xl font-bold">Silk Weave</p>;
+  return (
+    <div className="flex items-center gap-2">
+      <Image src="/image/icon.svg" alt="Silk Weave" width={32} height={32} />
+      <p className="text-2xl font-bold">Silk Weave</p>
+    </div>
+  );
 };
