6b674caa2cc502d0d54adf79f1f2522bc6fa7ad9
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-27 01:24:26 +0900

    fix: sidebarに仮データ配置

--- src/components/parts/sidebar.tsx
+++ src/components/parts/sidebar.tsx
@@ -1,8 +1,26 @@
+import { Button } from "../ui/button";
+import { PlusCircleIcon } from "lucide-react";
+
 export const Sidebar = () => {
   return (
-    <div className="h-full p-4">
+    <div className="p-4">
       <div className="border-b-2 border-dashed">
         <p className="text-2xl font-bold">Channels</p>
+      </div>
+      <div className="flex flex-col gap-2 py-4">
+        <Button variant="ghost" align="left">
+          # aaa
+        </Button>
+        <Button variant="ghost" align="left">
+          # aaaccc
+        </Button>
+        <Button variant="ghost" align="left">
+          # aaabb
+        </Button>
+        <Button variant="outline" className="flex gap-2">
+          <PlusCircleIcon className="w-4 h-4" />
+          Add Channel
+        </Button>
       </div>
     </div>
   );
