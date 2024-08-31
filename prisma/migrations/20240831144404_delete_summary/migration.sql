0d3ae04afe6aed2d1e3ed940f696e7e82d5b6225
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-31 23:47:57 +0900

    fix: schemeにthreadsの開始ユーザーを追加

---
+++ prisma/migrations/20240831144404_delete_summary/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `summary` on the `Thread` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Thread" DROP COLUMN "summary";
