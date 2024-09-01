01eb5b995bb418fa1d276605b626f8dc10dbb9a2
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-02 01:33:47 +0900

    fix: 開始ユーザーの表示位置を修正

--- src/components/page/top/_components/thread-card.tsx
+++ src/components/page/top/_components/thread-card.tsx
@@ -14,8 +14,10 @@ export const ThreadCard = ({ title, user, tags, updatedAt }: Props) => {
       className="flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent relative"
     >
       <p className="absolute top-2 right-2 text-xs text-muted-foreground">{updatedAt.toLocaleDateString()}</p>
-      <p className="font-bold">{title}</p>
-      <p className="line-clamp-2 text-muted-foreground">Started by {user}</p>
+      <div className="flex flex-wrap gap-2">
+        <p className="font-bold">{title}</p>
+        <p className="line-clamp-2 text-muted-foreground">by {user}</p>
+      </div>
       <div className="flex gap-2">
         {tags.map((tag) => (
           <Badge key={tag} variant="outline">
