f2e83ce0d37f0d7de8b09f3e093209ba3dd183a2
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-02 01:24:24 +0900

    fix: チャンネル選択をcomboboxとして実装

--- src/components/parts/sidebar/_components/create-channel-form.tsx
+++ src/components/parts/sidebar/_components/create-channel-form.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { useToast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
 import { ActionStatus } from "@/types/action-status";
 import { type Channel, ChannelSchema } from "@/types/zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
 import { useEffect, useRef, useTransition } from "react";
 import { useFormState } from "react-dom";
 import { useForm } from "react-hook-form";
@@ -57,6 +60,12 @@ export const CreateChannelForm: React.FC<CreateChannelFormProps> = ({ setDialogO
     }
   }, [state.status]);
 
+  const channels = [
+    { value: "apple", label: "Apple" },
+    { value: "banana", label: "Banana" },
+    { value: "blueberry", label: "Blueberry" },
+  ];
+
   return (
     <Form {...form}>
       <form ref={formRef} className="flex flex-col gap-4" onSubmit={form.handleSubmit(handleFormAction)}>
@@ -65,18 +74,49 @@ export const CreateChannelForm: React.FC<CreateChannelFormProps> = ({ setDialogO
           name="name"
           render={({ field }) => (
             <FormItem>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a channel" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="apple">Apple</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                </SelectContent>
-              </Select>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      className={cn("w-full justify-between", !field.value && "text-muted-foreground")}
+                    >
+                      {field.value
+                        ? channels.find((channel) => channel.value === field.value)?.label
+                        : "Select a channel"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-96 p-0">
+                  <Command>
+                    <CommandInput placeholder="Search channel..." />
+                    <CommandList>
+                      <CommandEmpty>No channel found.</CommandEmpty>
+                      <CommandGroup>
+                        {channels.map((channel) => (
+                          <CommandItem
+                            value={channel.label}
+                            key={channel.value}
+                            onSelect={() => {
+                              form.setValue("name", channel.value);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                channel.value === field.value ? "opacity-100" : "opacity-0",
+                              )}
+                            />
+                            {channel.label}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
               <FormMessage />
             </FormItem>
           )}
