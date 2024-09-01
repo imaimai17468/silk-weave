50d936464c27289a44f97ad02d6d9f424b6398b3
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-01 20:07:24 +0900

    feat: channelのapi routeを実装

--- src/app/api/channel/route.ts
+++ src/app/api/channel/route.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { ChannelSchema } from "@/types";
-
 const prisma = new PrismaClient();
 
 export const connect = async () => {
@@ -21,8 +20,8 @@ export const GET = async () => {
 
     if (!parsedChannels.success) {
       return NextResponse.json(
-        { messeage: `Error: ${parsedChannels.error}` },
-        { status: 500 }
+        { messeage: `Parsing Error: ${parsedChannels.error}` },
+        { status: 400 }
       );
     }
 
@@ -41,20 +40,21 @@ export const POST = async (req: Request) => {
   try {
     await connect();
     const { name } = await req.json();
-    const channel = await prisma.channel.create({
-      data: { name },
-    });
 
-    const parsedChannel = ChannelSchema.safeParse(channel);
+    const nameValidation = ChannelSchema.pick({ name }).safeParse({ name });
 
-    if (!parsedChannel.success) {
+    if (!nameValidation.success) {
       return NextResponse.json(
-        { messeage: `Error: ${parsedChannel.error}` },
-        { status: 500 }
+        { message: `Validation Error: ${nameValidation.error}` },
+        { status: 400 }
       );
     }
 
-    return NextResponse.json({ channel: parsedChannel.data }, { status: 200 });
+    const channel = await prisma.channel.create({
+      data: { name: nameValidation.data.name },
+    });
+
+    return NextResponse.json({ channel }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ messeage: `Error: ${error}` }, { status: 500 });
   } finally {
