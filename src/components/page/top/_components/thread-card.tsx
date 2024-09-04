c2719e660310f9b52e56fc0844922e84cfe4c5e1
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-05 01:53:21 +0900

    fix: thread-cardがナビゲーションできるよう実装

--- src/components/page/top/_components/thread-card.tsx
+++ src/components/page/top/_components/thread-card.tsx
@@ -1,30 +1,32 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 type Props = {
   title: string;
   user: string;
   tags: string[];
   updatedAt: Date;
+  href: string;
 };
 
-export const ThreadCard = ({ title, user, tags, updatedAt }: Props) => {
+export const ThreadCard = ({ title, user, tags, updatedAt, href }: Props) => {
   return (
-    <button
-      type="button"
-      className="flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent relative"
-    >
-      <p className="absolute top-2 right-2 text-xs text-muted-foreground">{updatedAt.toLocaleDateString()}</p>
-      <div className="flex flex-wrap gap-2">
-        <p className="font-bold">{title}</p>
-        <p className="line-clamp-2 text-muted-foreground">by {user}</p>
-      </div>
-      <div className="flex gap-2">
-        {tags.map((tag) => (
-          <Badge key={tag} variant="outline">
-            {tag}
-          </Badge>
-        ))}
-      </div>
-    </button>
+    <Button variant="outline" className="h-auto items-start" asChild>
+      <Link href={href} className="flex flex-col relative gap-4">
+        <p className="absolute top-2 right-2 text-xs text-muted-foreground">{updatedAt.toLocaleDateString()}</p>
+        <div className="flex flex-wrap gap-2">
+          <p className="font-bold">{title}</p>
+          <p className="line-clamp-2 text-muted-foreground">by {user}</p>
+        </div>
+        <div className="flex gap-2">
+          {tags.map((tag) => (
+            <Badge key={tag} variant="outline">
+              {tag}
+            </Badge>
+          ))}
+        </div>
+      </Link>
+    </Button>
   );
 };
