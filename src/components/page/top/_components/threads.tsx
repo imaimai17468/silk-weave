d8e7e56f099926e9e2a08dfd5fb4aa6f14d51e77
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-05 01:52:50 +0900

    fix: channelがない場合のthreads表示を追加

--- src/components/page/top/_components/threads.tsx
+++ src/components/page/top/_components/threads.tsx
@@ -1,21 +1,30 @@
-import { Input } from "@/components/ui/input";
-import { ThreadCard } from "./thread-card";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { searchParamsCache, serialize } from "@/utils/searchParams";
+import { EyeOff } from "lucide-react";
+import { ThreadList } from "./thread-list";
 
 export const Threads = () => {
+  const { channelId } = searchParamsCache.all();
+  const setThreadId = (id: string) => {
+    return serialize("/", { channelId, threadId: id });
+  };
+
   return (
     <div className="p-4">
       <div className="border-b-2 border-dashed">
         <p className="text-2xl font-bold">Threads</p>
       </div>
-      <div className="flex flex-col gap-4 py-4">
-        <Input placeholder="Search" />
-        <div className="flex flex-col gap-4">
-          <ThreadCard title="〇〇についてスレッド" user="いまいまい" tags={["hoge", "fuga"]} updatedAt={new Date()} />
-          <ThreadCard title="〇〇についてスレッド" user="いまいまい" tags={["hoge", "fuga"]} updatedAt={new Date()} />
-          <ThreadCard title="〇〇についてスレッド" user="いまいまい" tags={["hoge", "fuga"]} updatedAt={new Date()} />
-          <ThreadCard title="〇〇についてスレッド" user="いまいまい" tags={["hoge", "fuga"]} updatedAt={new Date()} />
+      {channelId ? (
+        <ThreadList channelId={channelId} onSelect={setThreadId} />
+      ) : (
+        <div className="py-4">
+          <Alert>
+            <EyeOff className="w-4 h-4" />
+            <AlertTitle>No Threads</AlertTitle>
+            <AlertDescription>Please select a channel</AlertDescription>
+          </Alert>
         </div>
-      </div>
+      )}
     </div>
   );
 };
