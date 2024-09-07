a270f0034c7135fc4bb1fdaf262a2e8ee237af38
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-08 04:18:44 +0900

    feat: channelの取得部分をhonoに対応

--- src/components/parts/sidebar/_libs/get-channels.ts
+++ src/components/parts/sidebar/_libs/get-channels.ts
@@ -1,13 +1,18 @@
+import { client } from "@/lib/hono";
 import type { Channel } from "@/types/zod";
+import { map } from "remeda";
 
 export const getChannels = async (): Promise<Channel[] | undefined> => {
   try {
-    const res = await fetch("http://localhost:3000/api/channel");
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const json = await res.json();
-    return json.channels;
+    const res = await client.api.channel.$get().then((res) => res.json());
+
+    const channels = map(res, (channel) => ({
+      ...channel,
+      createdAt: new Date(channel.createdAt),
+      updatedAt: new Date(channel.updatedAt),
+    }));
+
+    return channels;
   } catch (error) {
     console.error("Failed to fetch channels:", error);
     return undefined;
