8b1c4b08c5736d7e4bdb99d1756b8dc0def79b28
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-08 04:24:03 +0900

    feat: get-channelのエラーハンドリングを実装

--- src/components/parts/sidebar/_libs/get-channels.ts
+++ src/components/parts/sidebar/_libs/get-channels.ts
@@ -1,18 +1,30 @@
 import { client } from "@/lib/hono";
 import type { Channel } from "@/types/zod";
+import { ChannelSchema } from "@/types/zod";
 import { map } from "remeda";
 
 export const getChannels = async (): Promise<Channel[] | undefined> => {
   try {
-    const res = await client.api.channel.$get().then((res) => res.json());
+    const res = await client.api.channel
+      .$get()
+      .then((res) => res.json())
+      .catch((error) => {
+        throw new Error(`Failed to fetch channels: ${error}`);
+      });
 
-    const channels = map(res, (channel) => ({
+    const data = map(res, (channel) => ({
       ...channel,
       createdAt: new Date(channel.createdAt),
       updatedAt: new Date(channel.updatedAt),
     }));
 
-    return channels;
+    const channels = ChannelSchema.array().safeParse(data);
+
+    if (!channels.success) {
+      throw new Error("Failed to parse channels");
+    }
+
+    return channels.data;
   } catch (error) {
     console.error("Failed to fetch channels:", error);
     return undefined;
