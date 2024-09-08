622e352bc4fb6cb433b060f4458a03edb22cf3bb
imaimai17468 <tosiki_801_life@outlook.jp>
2024-09-08 15:00:11 +0900

    feat: 環境変数をt3-envで管理

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,5 +1,6 @@
 import { hc } from "hono/client";
 
 import type { AppType } from "@/app/api/[[...route]]/route";
+import { env } from "@/env.mjs";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL || "");
+export const client = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
