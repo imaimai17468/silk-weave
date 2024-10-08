9df42ab5cdf3bc4661fda5a03484e63579a68f6b
imaimai17468 <tosiki_801_life@outlook.jp>
2024-10-08 23:54:57 +0900

    fix: キャッシュ処理を追加

--- src/utils/get-iap-token.ts
+++ src/utils/get-iap-token.ts
@@ -1,10 +1,20 @@
 import { GoogleAuth } from "google-auth-library";
 
 const IAP_CLIENT_ID = process.env.IAP_CLIENT_ID;
+const TOKEN_EXPIRATION_BUFFER = 60;
+
+let cachedToken: string | null = null;
+let tokenExpirationTime: number | null = null;
 
 export const getIapTokenResponse = async (): Promise<string> => {
   if (!IAP_CLIENT_ID) {
     throw new Error("Missing required environment variables");
+  }
+
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  if (cachedToken && tokenExpirationTime && currentTime < tokenExpirationTime - TOKEN_EXPIRATION_BUFFER) {
+    return cachedToken;
   }
 
   const auth = new GoogleAuth();
@@ -15,5 +25,9 @@ export const getIapTokenResponse = async (): Promise<string> => {
     throw new Error("tokenResponse.Authorization is undefined");
   }
 
-  return tokenResponse.Authorization;
+  tokenExpirationTime = currentTime + 3600;
+
+  cachedToken = tokenResponse.Authorization;
+
+  return cachedToken;
 };
